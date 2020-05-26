86d76c22dc899c40862b2735d337e3ec7d013513
isabroch <isabrochweb@gmail.com>
2020-05-26 12:59:07 +0200

    add monsterrat font from google

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,29 @@ import Helmet from "react-helmet";
 import "../components/reset.css";
 import { css } from "@emotion/core";
 import AspectRatio from "../components/AspectRatio";
+import BookmarkArticle from "../components/BookmarkArticle";
 
 const RootIndex = (props) => {
   const siteTitle = props.data.site.siteMetadata.title;
+  const article = {
+    title: "Virtual Reality",
+    category: { name: "Technology", color: "#F0F0F0" },
+    img: { src: "https://placekitten.com/200/300", alt: "Kitty!" },
+  };
 
   return (
     <div
       css={css`
-        font-weight: bold;
+        font-family: "Montserrat", sans-serif;
       `}
     >
-      <Helmet title={siteTitle} />
-      <AspectRatio
-        ratio={16 / 9}
-        background="yellow"
-        rounded
-        imgFit="cover"
-        maxSize={{ width: "200px" }}
-      >
-        <img src="http://placehold.it/1000" alt="" />
-      </AspectRatio>
+      <Helmet title={siteTitle}>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <BookmarkArticle article={article} />
     </div>
   );
 };
