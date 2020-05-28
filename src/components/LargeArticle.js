df03c0a2e64fb7a401d31b79acece33fde2be609
isabroch <isabrochweb@gmail.com>
2020-05-28 11:27:59 +0200

    Show text content

--- src/components/LargeArticle.js
+++ src/components/LargeArticle.js
@@ -28,13 +28,69 @@ export const LargeArticle = ({
   article: { title, images, category, description },
   colSpan = 8,
 }) => {
+  const style = {
+    wrapper: css`
+      display: grid;
+      padding: 44px 48px 52px;
+      grid-template-columns: 5fr 7fr;
+      grid-gap: 20px;
+      height: 100%;
+      box-sizing: border-box;
+    `,
+    mainImage: css`
+      border-radius: 10px;
+      height: 100%;
+    `,
+    content: css`
+      display: flex;
+      flex-direction: column;
+    `,
+    extraImages: css`
+      margin-top: auto;
+      padding-top: 1rem;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      gap: 10px;
+    `,
+  };
+
   return (
     <Card colSpan={colSpan}>
-      <AspectRatio imgFit="cover" ratio={293 / 375} rounded>
-        <Img fluid={images[0].fluid} />
-      </AspectRatio>
+      <article css={style.wrapper}>
+        <Img fluid={images[0].fluid} css={style.mainImage} />
 
-      <ExtraImages images={images} />
+        <div css={style.content}>
+          <Subtitle
+            padding
+            background={category.color}
+            color={"#ffffff"}
+            css={css`
+              align-self: flex-end;
+            `}
+          >
+            {category.name}
+          </Subtitle>
+          <Title
+            size="l"
+            css={css`
+              padding: 1rem 0;
+            `}
+          >
+            {title}
+          </Title>
+          <Blurb lines={4} color="#7B8591">
+            {description}
+          </Blurb>
+          <div css={style.extraImages}>
+            <ExtraImages
+              images={images}
+              css={css`
+                margin-top: auto;
+              `}
+            />
+          </div>
+        </div>
+      </article>
     </Card>
   );
 };
